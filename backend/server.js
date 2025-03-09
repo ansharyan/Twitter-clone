74ee02201c34dac438af8684ca884bdb95eae528
anshryan <ansharyan9999@gmail.com>
2025-03-09 22:58:08 +0530

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRouter from "./routes/auth_route.js"
 import dotenv from "dotenv"
@@ -17,6 +18,7 @@ cloudinary.config({
 
 const app = express();
 const PORT=process.env.PORT || 8000;
+const __dirname = path.resolve();
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json({limit: "5mb"})); // to parse req.body
@@ -27,6 +29,13 @@ app.use("/api/auth", authRouter);
 app.use('/api/user', userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/notifications", notificationRouter)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+};
 
 const connectdb = async () =>{
     try {
